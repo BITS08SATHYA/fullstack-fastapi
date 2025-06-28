2eaac812ec4e2b0d771c4080e12f224dc34a9d21
Sathya <neelasathya06@gmail.com>
2025-06-29 01:08:40 +0530

    predefined values impl

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
-
-
 
 @app.get('/')
 def index():
     return {'message': 'Hello World'}
 
-@app.get('/blog/all')
-def get_all_blog():
-    return {'message': 'All blogs provided'}
+# class BlogType(str, Enum):
+#     short: 'short'
+#     story: 'story'
+#     howto: 'howto'
+# @app.get('/blog/type/{type}')
+# def get_blog_type(type: BlogType):
+#     return {'message': f'Blog type: {type.value}'}
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {"message": f"Blog type: {type.value}"}
+
+
+
+# @app.get('/blog/all')
+# def get_all_blog():
+#     return {'message': 'All blogs provided'}
+#
+# @app.get('/blog/{id}')
+# def get_blog(id: int):
+#     return {'message': f'Blog with id {id}'}
