7486b5b140f5297a0eacd048cefd0f9b0956dbbb
Sathya <neelasathya06@gmail.com>
2025-07-19 19:41:48 -0400

    query parameters impl

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -15,9 +16,13 @@ def index():
 # def get_blog_type(type: BlogType):
 #     return {'message': f'Blog type: {type.value}'}
 
+# @app.get('/blog/all')
+# def get_all_blog():
+#     return {'message': 'All blogs provided'}
+
 @app.get('/blog/all')
-def get_all_blog():
-    return {'message': 'All blogs provided'}
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
 
 class BlogType(str, Enum):
     short = 'short'
@@ -28,18 +33,14 @@ class BlogType(str, Enum):
 def get_blog_type(type: BlogType):
     return {"message": f"Blog type: {type.value}"}
 
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
 
 @app.get('/blog/{id}')
 def get_blog(id: int):
     return {'message' : f'Blog with id: {id}'}
 
 
-# @app.get('/blog/all')
-# def get_all_blogs(page=1, page_size=10):
-#     return {'message': f'All {page_size} blogs on page {page}'}
 
-
-#
-# @app.get('/blog/{id}')
-# def get_blog(id: int):
-#     return {'message': f'Blog with id {id}'}
