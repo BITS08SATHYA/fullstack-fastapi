0af0d9c3d7b5cdf3b9f9ba8041af77dcbf39c273
Sathya <neelasathya06@gmail.com>
2025-07-21 14:18:41 -0400

    request body of post request impl

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from typing import Optional
 router = APIRouter(
     prefix="/blog",
     tags=["blog"],
 )
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+
+
 @router.post('/')
-def create_blog():
-    pass
+def create_blog(blog: BlogModel):
+    return {'data': blog}
