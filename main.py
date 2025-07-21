95d0f3ab72907c5f81c7f9aa72695cefcf752b0a
Sathya <neelasathya06@gmail.com>
2025-07-20 21:28:59 -0400

    summary and description impl

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def index():
 
 @app.get('/blog/all',
          tags=['blog'],
+         summary="Retrieve all blogs",
+         description="This api call simulates fetching all blogs"
          )
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
@@ -37,6 +39,15 @@ def get_blog_type(type: BlogType):
 
 @app.get('/blog/{id}/comments/{comment_id}', tags = ['blog','comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates reterieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+
+    """
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 
