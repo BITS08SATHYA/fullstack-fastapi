614ecdea21c1a9969b7a3e8528b085b810c3981e
Sathya <neelasathya06@gmail.com>
2025-08-04 21:16:59 -0400

    successfully written to database

--- db/db_user.py
+++ db/db_user.py
@@ -1,11 +1,16 @@
 from sqlalchemy.orm.session import Session
-from Schemas import UserBase
+from db.schemas import UserBase
 from db.models import DbUser
+from db.hash import Hash
 
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=
+        password=Hash.bcrypt(request.password)
     )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from db.database import engine
 from router import blog_get
 from router import blog_post
 from db import models
+from router import user
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 @app.get('/')
